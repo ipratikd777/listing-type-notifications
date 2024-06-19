f4cc31d771024af71d2d833951546b7eb52b3624
ipratikd777 <ipratik.d777@gmail.com>
2024-06-20 01:05:52 +0530

    Add files via upload

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,1 +1,41 @@
+jQuery(document).ready(function($) {
+  // On click clear button all user
+  $(document).on('click', 'a.clear-all-notifications', function(e) {
+    e.preventDefault();
+    var currId = $(this).data('currentuid');
+    $.ajax({
+        url: ajaxurl,
+        type: 'POST',
+        data: {
+            action: 'lt_clear_notification',
+            currentId : currId,
+        },
+        success: function(response) {
+          if (response.success) {
+            $('.user-notifications').empty();
+          } else {
+              alert('Failed to clear all notifications.');
+          }
+        }
+    });
+  });
 
+  // On click clear button for admin
+  $(document).on('click', 'a.admin-clear-all-notifications', function(e) {
+    e.preventDefault();
+    $.ajax({
+        url: ajaxurl,
+        type: 'POST',
+        data: {
+            action: 'lt_admin_clear_notification',
+        },
+        success: function(response) {
+          if (response.success) {
+            $('.admin-notifications').empty();
+          } else {
+              alert('Failed to clear all notifications.');
+          }
+        }
+    });
+  });
+});
